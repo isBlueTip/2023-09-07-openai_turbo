c67029e7362f4c191344aa822e5093271e00b62c
Simon Egorov <rhinorofl@gmail.com>
2023-09-08 00:05:04 +0700

    update handlers

--- handlers.py
+++ handlers.py
@@ -1,3 +1,16 @@
 from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
 
 router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("я отвечу твоим id на любое сообщение")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    await msg.answer(f"твой id :{msg.from_user.id}>")
