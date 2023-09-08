ac25c765d77648559dbbf7c35105e6f685847f42
Simon Egorov <rhinorofl@gmail.com>
2023-09-08 13:07:32 +0700

    Update text constants

--- text_constants.py
+++ text_constants.py
@@ -1,2 +1,16 @@
-greet = "Привет, {name}, я интерфейс для работы с OpenAI 3.5 Turbo и Dall-e."
-menu = " < Главное меню"
+greet = "Привет, {name}, я интерфейс для работы с OpenAI 3.5 Turbo"  # message text template
+
+
+menu = "Главное меню"  # button text template
+generate_text = "Сделать запрос для генерации текста"  # button text template
+
+gen_exit = "Для выхода в меню нажми кнопку ниже"
+generation_error = (
+    "Ошибка генерации. Возможные причины: \n"
+    "1. Перегружены сервера OpenAI;\n"
+    "2. Запрос нарушил правила OpenAI;\n"
+    "3. Ошибка в работе бота;\n"
+)
+generation_waiting = "Ожидание ответа от сервера openai"  # message text template
+
+err = "Что-то пошло не так"
